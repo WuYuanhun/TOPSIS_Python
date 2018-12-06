fd028f6bfac2d09690612bda3511c997d259fdc9
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2018-12-06 08:30:43 +0800

    complete library

--- topsis.py
+++ topsis.py
@@ -5,6 +5,13 @@
 
 import numpy as np
 
+def calEuclidDis(vector):
+    sum = 0
+    for x in vector:
+        sum += (x**2)
+    
+    return np.sqrt(sum)
+
 class topsisMatrix():
 
     
@@ -124,3 +131,83 @@ class topsisMatrix():
             raise Exception("Out of Range")
         else:
             return self.kname[idx]
+    
+    def genForMat(self):
+        self.fotMat = self.topNaMat
+        self.size()
+        for obj in range(0, self.numCrt):
+            base = calEuclidDis([self.topNaMat[x][obj] for x in range(0,self.numObj)])
+            print(self.knameAt(obj)+" base: "+str(base))
+            for v in range(0,self.numObj):
+                self.fotMat[v][obj] /= base
+                print(self.fotMat[v][obj],end=' ')
+            print()
+        return self.fotMat
+
+    ''' generate weight formulate matrix'''
+    def genWgtMat(self):
+        self.wgtMat = self.fotMat
+
+        if len(self.weight) != self.numCrt:
+            self.weight = np.ones(shape=self.numCrt,dtype=np.float64)
+        
+        for obj in self.wgtMat:
+            idx = 0
+            for crit in obj:
+                crit *= self.weight[idx]
+                idx += 1
+        
+        return self.wgtMat
+
+    def solIdealSolution(self):
+        self.idealSolution = []
+        for x in range(0,self.numCrt):
+            nBig = self.wgtMat[0][x]
+            for i in range(0,self.numObj):
+                nBig = max(self.wgtMat[i][x],nBig)
+            self.idealSolution.append(nBig)
+
+    def readObjName(self,data):
+        self.objName = data
+
+    def run(self):
+        print(self.genEvaMat())
+        print("genEvaMat Comp")
+        print(self.genFotMat())
+        print("genFotMat Comp")
+        print(self.genWgtMat())
+        print("genWgtMat Comp")
+
+def store(data,filename="./data/temp.csv",type="default",header=None):
+    
+    if type=="matrix":
+        with open(filename,'w') as f:
+            if header is not None:
+                for x in header:
+                    x +='      '
+                    print("%.5s"%x,end='   ')
+                    f.write("%.5s   "%x)
+                f.write('\n')
+                print()
+                    
+            for obj in data:
+                for x in obj:
+                   print("%5.3f"%x,end='    ')
+                   f.write("%5.3f   "%x)
+                print()
+                f.write('\n')
+
+def DEEPmain():
+    #DATASET
+    _DATASET = []
+    dataset = _DATASET #import dataset here
+    tMatrix = topsisMatrix(data=dataset[0],mode="all")
+    tMatrix.readObjName(dataset[1])
+    tMatrix.run()
+    return tMatrix
+    
+
+if __name__ == "__main__":
+    # tMatrix = main()
+    # tMatrix.run()
+    tMatrix = DEEPmain()
